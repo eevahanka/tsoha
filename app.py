c8391c2a270b205553f3b883e9dc66c61b8ed9fb
eevahanka <eeva.hanka@iki.fi>
2021-11-18 17:01:38 +0200

    users

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
+from flask import render_template, request, redirect
 from flask import Flask
-from flask import render_template
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+
+import users
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", username = users.username())
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            #salasanat ei täsmää
+            return render_template("error.html")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            
+            return render_template("error.html")
---
+++ users.py
@@ -0,0 +1,38 @@
+from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login(username, password):
+    result = db.session.execute("SELECT id, password FROM users WHERE username=:username", {"username": username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
+
+def logout():
+    del session["user_id"]
+
+
+def register(username, password):
+    hash = generate_password_hash(password)
+    try:
+        db.session.execute("INSERT INTO users (username,password,type) VALUES (:username,:password,:type)", {
+                           "username": username, "password": hash, "type": "user"})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+
+def user_id():
+    return session.get("user_id", 0)
+
+def username():
+    result = db.session.execute("SELECT username FROM users WHERE id=:id", {"id": user_id()})
+    name = str(result.fetchone())[2:-3]
+    return name
