1d8bbcc83df3185bfe08ac1e7b1733f21e41181d
eevahanka <eeva.hanka@iki.fi>
2021-11-19 06:31:45 +0200

    fixing a typo

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ from os import getenv
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = app.config["SQLALCHEMY_DATABASE_URI"].uri.replace(
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config["SQLALCHEMY_DATABASE_URI"].replace(
         "postgres://", "postgresql://", 1)
 db = SQLAlchemy(app)
