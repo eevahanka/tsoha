d9c2889833cacd3e189408a07f86972207e1f860
eevahanka <eeva.hanka@iki.fi>
2021-12-03 00:40:58 +0200

    created needed database tables

--- schema.sql
+++ schema.sql
@@ -4,3 +4,31 @@ CREATE TABLE users (
     password TEXT, 
     type TEXT
 );
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    topic_name TEXT, 
+    visible BOOLEAN
+);
+CREATE TABLE chains (
+    id SERIAL PRIMARY KEY, 
+    chain_name TEXT,
+    chain_message, TEXT
+    creater_id INTEGER REFERENCES users,
+    visible BOOLEAN,
+    created_at TIMESTAMP,
+    topic INTEGER REFERENCES topics 
+);
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    sender INTEGER REFERENCES users,
+    content TEXT,
+    send_at TIMESTAMP,
+    visible BOOLEAN,
+    chain INTEGER REFERENCES chains
+)
+
+CREATE TABLE logins ( 
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    login_time TIMESTAMP
+)
